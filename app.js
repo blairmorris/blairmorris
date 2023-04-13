8a1dc81f2a889ebd28038277e532ee4f752478b5
Blair Morris <blair.morris@moodys.com>
2023-04-13 19:45:45 -0400

    Updating packages to latest + npm audit fixes. Removing old express middleware.

--- app.js
+++ app.js
@@ -1,41 +1,19 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
 
-/**
- * Module dependencies.
- */
-
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
-var mail = require('nodemailer');
-var contact = require('./modules/contact.js');
-
-var app = express();
+const app = express();
+const contact = require('./modules/contact.js');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.use(express.favicon());
-app.use(express.logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
-app.use(express.session());
-app.use(app.router);
+app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-//app.get('/', routes.index);
-//app.get('/users', user.list);
-
 app.post('/contact', contact.mail);
-console.log(contact.mail);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), () => {
+	console.log('Express server listening on port ' + app.get('port'));
 });
+
--- modules/contact.js
+++ modules/contact.js
@@ -1,19 +1,36 @@
-var mail = require('nodemailer').mail;
+const nodemailer = require('nodemailer');
+let testAccount, transporter;
+(async () => {
+    testAccount = await nodemailer.createTestAccount();
+    transporter = nodemailer.createTransport({
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false,
+        auth: {
+            user: testAccount.user,
+            pass: testAccount.pass,
+        }
+    })
+})()
 
-exports.mail = function(req, res) {
+exports.mail = async (req, res) => {
     console.log('email sending', req.body);
-    if(req.body.name && req.body.email && req.body.message){
-        mail({
-            from: req.body.name + "<"+req.body.email+">", // sender address
-            to: "edward@blairmorris.com", // list of receivers
-            subject: req.body.requestResume ? "Resume request from BlairMorris.com" : "Email from BlairMorris.com", // Subject line
-            text: req.body.message, // plaintext body
-            html: "<b>"+req.body.message+"</b>" // html body
-        }, function(error, response){
-            console.log('callback', error, response);
-        });
-        res.send(200);
-    } else {
-        res.send(400);
+    try {
+        if (req.body.name && req.body.email && req.body.message) {
+            const info = await transporter.sendMail({
+                from: `${req.body.name}<${req.body.email}>`, // sender address
+                to: "edward@blairmorris.com", // list of receivers
+                subject: req.body.requestResume ? 'Resume request from BlairMorris.com' : 'Email from BlairMorris.com', // Subject line
+                text: req.body.message, // plaintext body
+                html: `<b>${req.body.message}</b>`, // html body
+            })
+            console.log("Message sent: %s", info.messageId);
+            console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+            res.sendStatus(200);
+        } else {
+            res.sendStatus(400);
+        }
+    } catch (e) {
+        res.sendStatus(500);
     }
 };
